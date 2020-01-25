56bcdbc7c9ea3c85dd655bfce6f33e8a3a7db7e1
rinat <rinatkins@gmail.com>
2020-01-25 16:16:37 +0300

    feat: eslint + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import * as React from "react";
-import "./App.scss";
+import * as React from 'react';
+import './App.scss';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h1> Hello, World!</h1>
-      </div>
-    );
-  }
-}
+const App: React.FC = (): React.ReactElement => (
+  <div className='App'>
+    <h1> Hello, World!</h1>
+  </div>
+);
 
 export default App;
