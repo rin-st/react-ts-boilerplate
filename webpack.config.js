f8067277d813c9cb0748f1ca77eb510484db7909
rinat <rinatkins@gmail.com>
2020-02-11 00:03:20 +0300

    fix: index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,38 @@
-const path = require("path");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.tsx",
-  mode: "development",
-  devtool: "source-map",
+  entry: './src/index.tsx',
+  mode: 'development',
+  devtool: 'source-map',
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".scss"]
+    extensions: ['.ts', '.tsx', '.js', '.scss']
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    contentBase: path.join(__dirname, 'public/'),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/"
+    publicPath: 'http://localhost:3000/dist/'
   },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "./bundle.js"
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: './bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        loader: "ts-loader"
+        loader: 'ts-loader'
       },
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.js$/,
-        loader: "source-map-loader"
+        loader: 'source-map-loader'
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   }
